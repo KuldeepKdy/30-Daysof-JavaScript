cb736e0ebd4e6022bb206a4e42bbb01d1f26b5e8
KuldeepKdy <kuldeepkdy4@gmail.com>
2023-12-19 18:47:11 +0530

    To do list

---
+++ Day-7(To-Do List)/script.js
@@ -0,0 +1,26 @@
+const item = document.querySelector("#item");
+const toDoBox = document.querySelector("#to-do-box");
+
+item.addEventListener("keyup", function (event) {
+  //   console.log(event.key);
+  if (event.key == "Enter") {
+    // console.log(item.value);
+    addToDo(item.value);
+    item.value = "";
+  }
+});
+
+const addToDo = (item) => {
+  const listItem = document.createElement("li");
+  listItem.innerHTML = `
+  ${item}
+  <i class='fas fa-times'></i>
+  `;
+  listItem.addEventListener("click", function () {
+    this.classList.toggle("done");
+  });
+  listItem.querySelector("i").addEventListener("click", function () {
+    listItem.remove();
+  });
+  toDoBox.appendChild(listItem);
+};
