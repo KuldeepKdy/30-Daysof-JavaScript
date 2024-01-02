ec82fd98e8d6cd9566869cdf089d03b8ce0c200e
KuldeepKdy <kuldeepkdy4@gmail.com>
2024-01-02 10:04:53 +0530

    Toast Done

--- Day-24(Toast Notification)/script.js
+++ Day-24(Toast Notification)/script.js
@@ -2,11 +2,26 @@ let success = document.querySelectorAll("button");
 let toastBox = document.querySelector("#toastBox");
 
 success.forEach((data) => {
-  data.addEventListener("click", function showToast() {
+  data.addEventListener("click", function showToast(data) {
+    let msg = data.target.innerHTML;
     let toast = document.createElement("div");
     toast.classList.add("toast");
-    toast.innerHTML = "success";
+    if (msg.includes("Success")) {
+      toast.innerHTML = `✅ Successfully Submitted`;
+      toast.classList.add("success");
+    } else if (msg.includes("Error")) {
+      toast.innerHTML = `❌ Please fix the error!`;
+      toast.classList.add("error");
+    } else if (msg.includes("Invalid")) {
+      toast.innerHTML = `❗Invalid input, Check Again`;
+      toast.classList.add("invalid");
+    }
+    // toast.innerHTML = `now check msg ${msg}`;
     toastBox.appendChild(toast);
-    console.log("fxn is executing");
+    // console.log("fxn is executing");
+
+    setTimeout(() => {
+      toast.remove();
+    }, 5000);
   });
 });
